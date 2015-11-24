d360235d8eec10da98efd286b53d6e010794e40b
Gabriel Araújo <gabriel@startae.com.br>
2015-11-24 11:32:33 -0200

    update api link.

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default DS.RESTAdapter.extend(AuthenticatedRouteMixin,{
-  host: 'http://localhost:3000/api/v1',
+  host: 'http://dkanban.herokuapp.com/api/v1',
   headers: Ember.computed('session.data.authenticated.auth_token', function() {
     var authentication = this.get('session.data.authenticated');
     return{
